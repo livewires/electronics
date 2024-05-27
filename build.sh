8d2c11cb8ce2e695a9ebf082fd6e9521759c1c37
Joel Fergusson <joel@fergfamily.co.uk>
2024-05-27 10:15:55 +0100

    PR test

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+mkdir worksheets
+
 cd macropad/worksheet/
 # This is a bit of a cheat, there's probably a better way of doing it.
 cp -r ../../worksheet-components/* ./
 make
+
+cd ../../worksheets
+cp ../macropad/worksheet/worksheet.pdf ./macropad.pdf
