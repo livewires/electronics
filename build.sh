4cf1fd53c62c21094b330e20a878df7e8fa0069d
Joel Fergusson <joel@fergfamily.co.uk>
2026-02-07 17:28:52 +0000

    Added synth worksheet

--- build.sh
+++ build.sh
@@ -6,5 +6,11 @@ cd macropad/worksheet/
 cp -r ../../worksheet-components/* ./
 make
 
+cd synth/worksheet/
+# This is a bit of a cheat, there's probably a better way of doing it.
+cp -r ../../worksheet-components/* ./
+make
+
 cd ../../worksheets
 cp ../macropad/worksheet/worksheet.pdf ./macropad.pdf
+cp ../synth/worksheet/worksheet.pdf ./synth.pdf
