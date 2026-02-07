32815a503627bb8f096e04c7d88be5f98de05b5c
Joel Fergusson <joel@fergfamily.co.uk>
2026-02-07 17:39:20 +0000

    Updated build script

--- build.sh
+++ build.sh
@@ -5,12 +5,14 @@ cd macropad/worksheet/
 # This is a bit of a cheat, there's probably a better way of doing it.
 cp -r ../../worksheet-components/* ./
 make
+cd ../../
 
 cd synth/worksheet/
 # This is a bit of a cheat, there's probably a better way of doing it.
 cp -r ../../worksheet-components/* ./
 make
+cd ../../
 
-cd ../../worksheets
+cd worksheets
 cp ../macropad/worksheet/worksheet.pdf ./macropad.pdf
 cp ../synth/worksheet/worksheet.pdf ./synth.pdf
